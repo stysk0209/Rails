4c196a683f865c725ad29149bc89c739418f98dd
stysk0209 <stysk0209@outlook.jp>
2019-04-09 15:52:28 +0900

    [Add]sample_app追加

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'df2e4889caa19554938a256afe9f1250dc21a46723ccd1532cfd01f68851e975ff55bd9a741f3ae56fd22551ad79ab50a6f4e99ac401d6d521a1e91cc7bbcb95'
